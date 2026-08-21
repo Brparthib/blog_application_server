bb6bf60b07c327ce4bb35683b4c897d9d73d17ff
brparthib <brparthib@gmail.com>
2026-08-21 19:58:32 +0600

    fix: everything will change

--- src/modules/auth/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { AuthControllers } from "./auth.controllers";
-
-const router = Router();
-
-router.post("/login", AuthControllers.loginWithEmailAndPassword);
-router.post("/google", AuthControllers.authWithGoogle);
-
-export const AuthRoutes = router;
